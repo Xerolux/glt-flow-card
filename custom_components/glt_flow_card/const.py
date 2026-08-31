d182b8ee0380ec1ee49a222089794944d8f285d1
Basti <89860334+Xerolux@users.noreply.github.com>
2026-08-31 18:26:26 +0200

    Expand Companion constants for Platform 1.0

--- custom_components/glt_flow_card/const.py
+++ custom_components/glt_flow_card/const.py
@@ -1,5 +1,10 @@
 DOMAIN = "glt_flow_card"
-STORE_VERSION = 1
-STORE_KEY = "glt_flow_card.projects"
-MAX_AUDIT = 2000
+STORE_VERSION = 2
+STORE_KEY = "glt_flow_card.platform"
+MAX_AUDIT = 5000
 MAX_VERSIONS = 60
+DEFAULT_LOCK_TTL = 300
+SAFE_SERVICE_DOMAINS = {
+    "switch", "fan", "number", "select", "climate", "cover", "light",
+    "input_boolean", "input_number", "input_select", "water_heater", "button", "script"
+}
